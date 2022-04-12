d5715534f044f936c2dcda7c4a3df609efd5923e
MoosaKhan21 <91124952+MoosaKhan21@users.noreply.github.com>
2022-04-12 12:25:08 +0500

    Add files via upload

--- Bank-System.cpp
+++ Bank-System.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
-class User
-{
+class User{
 private:
     string name, account_num;
+    double balance;
+    int pin;
+    
+public:
+	double viewBalance(){
+		return balance;
+	}
+	void createAccount(User account){
+		getline(cin, account.name);
+		cin >> account.account_num;
+		cin >> account.balance;
+		cin >> account.pin;
+	}
+	void deposit(double addCash){
+		balance += addCash;
+	}
+	void withdraw(double subCash){
+		balance += subCash;
+	}
 };
+int main(){
+	user u1;
+	u1.createAccount()
+	u1.deposit(5000);
+	return 0;
+}
